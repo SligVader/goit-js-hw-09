95fc58eee9bd6a351fa3b4ba57c817696df75622
Volodymyr Titarenko <xsligvaderx@gmail.com>
2023-01-12 19:35:23 -0600

    task#1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let colorTimer = null;
+
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+
+function onStart() {
+  colorTimer = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+  }, 1000);
+
+  startBtn.setAttribute('disabled', true);
+  stopBtn.disabled = false;
+}
+
+function onStop() {
+  clearInterval(colorTimer);
+  stopBtn.setAttribute('disabled', true);
+  startBtn.disabled = false;
+}
