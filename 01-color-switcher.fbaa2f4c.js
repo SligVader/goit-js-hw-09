1b05d8f901f745b5628d8d9993af86f0bbbf7b7f
SligVader <109687460+SligVader@users.noreply.github.com>
2023-01-13 01:35:54 +0000

    Deploying to gh-pages from @ SligVader/goit-js-hw-09@95fc58eee9bd6a351fa3b4ba57c817696df75622 🚀

---
+++ 01-color-switcher.fbaa2f4c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;t.addEventListener("click",(function(){n=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor=t}),1e3),t.setAttribute("disabled",!0),e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(n),e.setAttribute("disabled",!0),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.fbaa2f4c.js.map
