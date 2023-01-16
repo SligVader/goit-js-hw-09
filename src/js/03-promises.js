145f7f88e76ad46d80f696667032ff97b58db307
Volodymyr Titarenko <xsligvaderx@gmail.com>
2023-01-15 20:52:09 -0600

    task#3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formRefs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+};
+
+formRefs.form.addEventListener('submit', promGen);
+
+function promGen(evt) {
+  evt.preventDefault();
+
+  let delay = Number(formRefs.delay.value);
+  const step = Number(formRefs.step.value);
+  const amount = Number(formRefs.amount.value);
+
+  for (let position = 0; position < amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }, delay)
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      }, delay);
+    delay += step;
+  }
+  evt.target.reset();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  const promise = { position, delay };
+
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        res(promise);
+      } else {
+        rej(promise);
+      }
+    });
+  });
 }
